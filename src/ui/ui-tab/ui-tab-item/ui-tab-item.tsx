8bfe0db50f0e36230160378789b431f526dacf6e
Qunite <lomak337@gmail.com>
2026-07-10 14:44:45 +0300

    added shadcn ui-lib and downloaded tooltip

--- src/ui/ui-tab/ui-tab-item/ui-tab-item.tsx
+++ src/ui/ui-tab/ui-tab-item/ui-tab-item.tsx
@@ -1,6 +1,12 @@
 import clsx from 'clsx';
 import './ui-tab-item.css';
+
 import { UiTabItemProps } from '@/src/ui/ui-tab/ui-tab-item/ui-tab-props';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/src/ui/ui-tooltip/tooltip';
 
 export function UiTabItem({
   title,
@@ -17,10 +23,22 @@ export function UiTabItem({
     className
   );
 
-  return (
+  const button = (
     <button className={classes} {...props}>
       {icon && <span className="flex items-center">{icon}</span>}
       {!isPinned && title}
     </button>
+  );
+
+  if (!isPinned) return button;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger render={button} />
+      <TooltipContent side="bottom" className="flex items-center gap-2">
+        {icon}
+        {title}
+      </TooltipContent>
+    </Tooltip>
   );
 }
